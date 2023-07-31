9192af6827cb8f28c7834a6e1a9e369c77d1f285
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2023-07-31 16:39:00 +0300

    Experemental code with Chakra!!!

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,40 +1,51 @@
-import { Outlet } from "react-router-dom";
-import { useColorMode, Button } from "@chakra-ui/react";
-import { FaSun, FaMoon } from "react-icons/fa";
-import { Container, Header, HeaderNav, Link } from "./SharedLayout.styled";
+import { Link as ReactRouterLink, Outlet } from 'react-router-dom';
+import { useColorMode, useColorModeValue, Button, Link as ChakraLink, Link } from '@chakra-ui/react';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import { Container, Header, HeaderNav} from './SharedLayout.styled';
+
 
 export const SharedLayout = () => {
     const { colorMode, toggleColorMode } = useColorMode();
+    
+    const bg = useColorModeValue('red.500', 'red.200')
+    const color = useColorModeValue('white', 'gray.800')
+    const textColor = useColorModeValue('gray.800', 'white')
 
-    return (
-        <Container>
-            <Header>
-                <div>
-                <Button colorScheme='teal' onClick={toggleColorMode}>
-                        {colorMode === 'light' ? <FaSun/> : <FaMoon/>}
-                </Button>
-                </div>
-                    
-                <div>
-                <HeaderNav>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                </HeaderNav>
-                <HeaderNav>
-                    <Link to="/registration">Register</Link>
-                    <Link to="/login">Log In</Link>
-                </HeaderNav>  
+  return (
+    <Container>
+      <Header>
+        <div>
+          <Button colorScheme="teal" onClick={toggleColorMode} bg={bg} color = {color}>
+            {colorMode === 'light' ? <FaSun /> : <FaMoon />}
+          </Button>
+        </div>
 
-                </div>
-
-            </Header>
-            <Outlet />
-            <footer>
-                <div>
-                    E-mail: <a href="mailto:golyaka.d@gmail.com">golyaka.d@gmail.com</a>
-                    Mobile: <a href="tel:+380974239084">+38 (097) 423-90-84</a>
-                </div>
-            </footer>
-        </Container>
-    );
+        <div>
+          <HeaderNav>
+            <ChakraLink as={ReactRouterLink} to="/" color = {textColor}>
+              Home
+            </ChakraLink>
+            <ChakraLink as={ReactRouterLink} to="/about" color = {textColor}>
+              About
+            </ChakraLink>
+          </HeaderNav>
+          <HeaderNav>
+            <ChakraLink as={ReactRouterLink} to="/registration" color = {textColor}>
+              Register
+            </ChakraLink>
+            <ChakraLink as={ReactRouterLink} to="/login" color = {textColor}>
+              Log In
+            </ChakraLink>
+          </HeaderNav>
+        </div>
+      </Header>
+      <Outlet />
+      <footer>
+        <div>
+          E-mail: <Link href="mailto:golyaka.d@gmail.com" >golyaka.d@gmail.com</Link>
+                  Mobile: <Link href="tel:+380974239084" _hover={{color: "teal.500"}}>+38 (097) 423-90-84</Link>
+        </div>
+      </footer>
+    </Container>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
-import theme from './components/thems';
+import theme from './thems/thems';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 import './index.css';
